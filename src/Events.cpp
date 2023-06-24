74bde4d0afda7a40084f99214491ab82aabaf4a6
Angad <angadmisra28@gmail.com>
2023-06-24 10:46:01 -0700

    Add null check

--- src/Events.cpp
+++ src/Events.cpp
@@ -12,37 +12,39 @@ namespace Events {
                                                              RE::BSTEventSource<RE::TESHitEvent>* a_eventSource) {
         if (!a_event) return RE::BSEventNotifyControl::kContinue;
 
-        if (a_event->cause->IsPlayerRef()) {
-            if (const auto chest = a_event->target->As<RE::TESObjectREFR>(); chest->IsLocked()) {
-                if (const auto player = RE::PlayerCharacter::GetSingleton();
-                    Utility::is_power_attacking(player->AsReference(), chest)) {
-                    const auto equipped_right = player->GetEquippedObject(false);
-                    if (const auto weapon = equipped_right->As<RE::TESObjectWEAP>();
-                        weapon->IsTwoHandedAxe() || weapon->IsTwoHandedSword()) {
-                        if (const auto lock = chest->GetLock(); lock->IsLocked()) {
-                            const auto level = lock->GetLockLevel(chest);
-                            const auto stamina = player->AsActorValueOwner()->GetActorValue(
-                                RE::ActorValue::kStamina
-                            );
-                            if (stamina > 100.0f && stamina <= 125.0f && level == RE::LOCK_LEVEL::kVeryEasy) {
-                                lock->SetLocked(false);
-                                player->AddSkillExperience(RE::ActorValue::kStamina, 17.5f);
-                            }
-                            if (stamina > 125.0f && stamina <= 175.0f && level == RE::LOCK_LEVEL::kEasy) {
-                                lock->SetLocked(false);
-                                player->AddSkillExperience(RE::ActorValue::kStamina, 25.0f);
-                            }
-                            if (stamina > 175.0f && stamina <= 225.0f && level == RE::LOCK_LEVEL::kAverage) {
-                                lock->SetLocked(false);
-                                player->AddSkillExperience(RE::ActorValue::kStamina, 40.0f);
-                            }
-                            if (stamina > 225.0f && stamina <= 275.0f && level == RE::LOCK_LEVEL::kHard) {
-                                lock->SetLocked(false);
-                                player->AddSkillExperience(RE::ActorValue::kStamina, 80.0f);
-                            }
-                            if (stamina >= 275 && level == RE::LOCK_LEVEL::kVeryHard) {
-                                lock->SetLocked(false);
-                                player->AddSkillExperience(RE::ActorValue::kStamina, 120.0f);
+        if (a_event->cause) {
+            if (a_event->cause->IsPlayerRef()) {
+                if (const auto chest = a_event->target->As<RE::TESObjectREFR>(); chest->IsLocked()) {
+                    if (const auto player = RE::PlayerCharacter::GetSingleton();
+                        Utility::is_power_attacking(player->AsReference(), chest)) {
+                        const auto equipped_right = player->GetEquippedObject(false);
+                        if (const auto weapon = equipped_right->As<RE::TESObjectWEAP>();
+                            weapon->IsTwoHandedAxe() || weapon->IsTwoHandedSword()) {
+                            if (const auto lock = chest->GetLock()) {
+                                const auto level = lock->GetLockLevel(chest);
+                                const auto stamina = player->AsActorValueOwner()->GetActorValue(
+                                    RE::ActorValue::kStamina
+                                    );
+                                if (stamina > 100.0f && stamina <= 125.0f && level == RE::LOCK_LEVEL::kVeryEasy) {
+                                    lock->SetLocked(false);
+                                    player->AddSkillExperience(RE::ActorValue::kStamina, 17.5f);
+                                }
+                                if (stamina > 125.0f && stamina <= 175.0f && level == RE::LOCK_LEVEL::kEasy) {
+                                    lock->SetLocked(false);
+                                    player->AddSkillExperience(RE::ActorValue::kStamina, 25.0f);
+                                }
+                                if (stamina > 175.0f && stamina <= 225.0f && level == RE::LOCK_LEVEL::kAverage) {
+                                    lock->SetLocked(false);
+                                    player->AddSkillExperience(RE::ActorValue::kStamina, 40.0f);
+                                }
+                                if (stamina > 225.0f && stamina <= 275.0f && level == RE::LOCK_LEVEL::kHard) {
+                                    lock->SetLocked(false);
+                                    player->AddSkillExperience(RE::ActorValue::kStamina, 80.0f);
+                                }
+                                if (stamina >= 275 && level == RE::LOCK_LEVEL::kVeryHard) {
+                                    lock->SetLocked(false);
+                                    player->AddSkillExperience(RE::ActorValue::kStamina, 120.0f);
+                                }
                             }
                         }
                     }
