1e498a08e951bc081dcaad4b29177559d25bb77e
Angad <angadmisra28@gmail.com>
2023-08-04 12:17:52 -0700

    Fix ini settings

--- src/Events.cpp
+++ src/Events.cpp
@@ -26,6 +26,7 @@ namespace Events {
                                     const auto lock = locked->GetLock();
                                     const auto level = lock->GetLockLevel(locked);
                                     logger::debug("Lock level: {}", static_cast<int>(level));
+
                                     const auto stamina_av = player->AsActorValueOwner()->GetActorValue(RE::ActorValue::kStamina);
                                     const auto two_handed_av = player->AsActorValueOwner()->GetActorValue(RE::ActorValue::kTwoHanded);
                                     const auto stamina_pct = Hooks::GetActorValuePercent(player->As<RE::Actor>(), RE::ActorValue::kStamina);
@@ -36,33 +37,26 @@ namespace Events {
                                     logger::debug("Current player two-handed: {}", two_handed);
 
                                     const auto av_to_increase = Settings::h2h_present ? RE::ActorValue::kPickpocket : RE::ActorValue::kLockpicking;
-
                                     auto cond1 = false, cond2 = false, cond3 = false, cond4 = false, cond5 = false;
-
                                     if (Settings::use_stamina && Settings::use_2h) {
                                         cond1 = stamina >= 100.0f && two_handed >= 20.0f && level == RE::LOCK_LEVEL::kVeryEasy;
                                         cond2 = stamina > 125.0f && two_handed > 30.0f && level == RE::LOCK_LEVEL::kEasy;
                                         cond3 = stamina > 175.0f && two_handed > 40.0f && level == RE::LOCK_LEVEL::kAverage;
                                         cond4 = stamina > 225.0f && two_handed > 60.0f && level == RE::LOCK_LEVEL::kHard;
                                         cond5 = stamina >= 275.0f && two_handed >= 80.0f && level == RE::LOCK_LEVEL::kVeryHard;
-                                    }
-
-                                    if (!Settings::use_stamina && Settings::use_2h) {
+                                    } else if (!Settings::use_stamina && Settings::use_2h) {
                                         cond1 = two_handed >= 20.0f && level == RE::LOCK_LEVEL::kVeryEasy;
                                         cond2 = two_handed > 30.0f && level == RE::LOCK_LEVEL::kEasy;
                                         cond3 = two_handed > 40.0f && level == RE::LOCK_LEVEL::kAverage;
                                         cond4 = two_handed > 60.0f && level == RE::LOCK_LEVEL::kHard;
                                         cond5 = two_handed >= 80.0f && level == RE::LOCK_LEVEL::kVeryHard;
-                                    }
-
-                                    if (Settings::use_stamina && !Settings::use_2h) {
+                                    } else if (Settings::use_stamina && !Settings::use_2h) {
                                         cond1 = stamina >= 100.0f && level == RE::LOCK_LEVEL::kVeryEasy;
                                         cond2 = stamina > 125.0f && level == RE::LOCK_LEVEL::kEasy;
                                         cond3 = stamina > 175.0f && level == RE::LOCK_LEVEL::kAverage;
                                         cond4 = stamina > 225.0f && level == RE::LOCK_LEVEL::kHard;
                                         cond5 = stamina >= 275.0f && level == RE::LOCK_LEVEL::kVeryHard;
                                     }
-
                                     if (cond1) {
                                         lock->SetLocked(false);
                                         logger::debug("Unlocked {}", locked->GetName());
@@ -76,8 +70,7 @@ namespace Events {
                                         }
                                         player->AddSkillExperience(av_to_increase, 17.5f);
                                         logger::debug("Added 17.5 XP");
-                                    }
-                                    if (cond2) {
+                                    } else if (cond2) {
                                         lock->SetLocked(false);
                                         logger::debug("Unlocked {}", locked->GetName());
                                         Hooks::FinalizeUnlock(locked->AsReference());
@@ -90,8 +83,7 @@ namespace Events {
                                         }
                                         player->AddSkillExperience(av_to_increase, 25.0f);
                                         logger::debug("Added 25 XP");
-                                    }
-                                    if (cond3) {
+                                    } else if (cond3) {
                                         lock->SetLocked(false);
                                         logger::debug("Unlocked {}", locked->GetName());
                                         Hooks::FinalizeUnlock(locked->AsReference());
@@ -104,8 +96,7 @@ namespace Events {
                                         }
                                         player->AddSkillExperience(av_to_increase, 40.0f);
                                         logger::debug("Added 40 XP");
-                                    }
-                                    if (cond4) {
+                                    } else if (cond4) {
                                         lock->SetLocked(false);
                                         logger::debug("Unlocked {}", locked->GetName());
                                         Hooks::FinalizeUnlock(locked->AsReference());
@@ -118,8 +109,7 @@ namespace Events {
                                         }
                                         player->AddSkillExperience(av_to_increase, 80.0f);
                                         logger::debug("Added 80 XP");
-                                    }
-                                    if (cond5) {
+                                    } else if (cond5) {
                                         lock->SetLocked(false);
                                         logger::debug("Unlocked {}", locked->GetName());
                                         Hooks::FinalizeUnlock(locked->AsReference());
--- src/Settings.cpp
+++ src/Settings.cpp
@@ -15,15 +15,15 @@ void Settings::LoadSettings() {
     ini.SetUnicode();
     ini.LoadFile(R"(.\Data\SKSE\Plugins\LockpickingForBarbarians.ini)");
 
-    debug_logging = ini.GetValue("Log", "Debug");
+    debug_logging = ini.GetBoolValue("Log", "Debug");
 
     if (debug_logging) {
         spdlog::get("Global")->set_level(spdlog::level::level_enum::debug);
         logger::debug("Debug logging enabled");
     }
 
-    use_stamina = ini.GetValue("General", "bUseStamina");
-    use_2h = ini.GetValue("General", "bUse2H");
+    use_stamina = ini.GetBoolValue("General", "bUseStamina");
+    use_2h = ini.GetBoolValue("General", "bUse2H");
 
     if (RE::TESDataHandler::GetSingleton()->LookupLoadedModByName("HandtoHand.esp"sv))
         h2h_present = true;
